6bdda13474c0cb1347849d0dc2634799d5328b2d
matystroia <stroiamatei@gmail.com>
2019-11-30 19:10:31 +0200

    Update day01.py

--- day01.py
+++ day01.py
@@ -1,4 +1,4 @@
 # lines = list(map(int, open('data.in', 'r').readlines()))
 with open('data.in', 'r') as f:
-    for line in [ln.strip() for ln in f.readlines()]:
+    for line in [line.strip() for line in f.readlines()]:
         pass
