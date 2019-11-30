b40bd4e3578098a1e8117e11633458778f852032
matystroia <stroiamatei@gmail.com>
2019-11-30 19:24:04 +0200

    add explicit list comprehension

--- day01.py
+++ day01.py
@@ -1,4 +1,5 @@
 # lines = list(map(int, open('data.in', 'r').readlines()))
 with open('data.in') as f:
-    for line in [line.strip() for line in f.readlines()]:
+    lines = [line.strip() for line in f.readlines()]
+    for line in lines:
         pass
