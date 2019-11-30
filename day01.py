cc3799e975d853849c525ce722752a938d81e564
matystroia <stroiamatei@gmail.com>
2019-11-30 19:19:44 +0200

    remove redundant file access mode

--- day01.py
+++ day01.py
@@ -1,4 +1,4 @@
 # lines = list(map(int, open('data.in', 'r').readlines()))
-with open('data.in', 'r') as f:
+with open('data.in') as f:
     for line in [line.strip() for line in f.readlines()]:
         pass
