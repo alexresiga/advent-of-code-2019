43d3d9933684e431e568a5a92225827c91f38b9d
alexresiga <resiga.alex@gmail.com>
2019-12-01 11:11:21 +0200

    update to python3.8 so i can use walrus operator

--- day02.py
+++ day02.py
@@ -1,13 +1,3 @@
 lines = list(map(int, open('data.in', 'r').readlines()))
 # with open('data.in', 'r') as f:
 #     for line in [line.strip() for line in f.readlines()]:
-
-# first
-# print(sum([x // 3 - 2 for x in lines]))
-
-# second
-_sum = 0
-for x in lines:
-    while (x := x // 3 - 2) > 0:
-        _sum += x
-print(_sum)
