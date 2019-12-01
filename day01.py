624619999e68c20071710aadb609d3d6b4352bca
alexresiga <resiga.alex@gmail.com>
2019-12-01 10:45:46 +0200

    add day 1

--- day01.py
+++ day01.py
@@ -1,4 +1,14 @@
-# lines = list(map(int, open('data.in', 'r').readlines()))
-with open('data.in', 'r') as f:
-    for line in [line.strip() for line in f.readlines()]:
-        pass
+lines = list(map(int, open('data.in', 'r').readlines()))
+# with open('data.in', 'r') as f:
+#     for line in [line.strip() for line in f.readlines()]:
+
+# first
+# print(sum([x // 3 - 2 for x in lines]))
+
+# second
+_sum = 0
+for x in lines:
+    while x // 3 - 2 > 0:
+        x = x // 3 - 2
+        _sum += x
+print(_sum)
