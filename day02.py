f476844fda0c6d07f4619197b518687a7c872287
alexresiga <resiga.alex@gmail.com>
2019-12-02 07:46:06 +0200

    add day 2

--- day02.py
+++ day02.py
@@ -1,3 +1,18 @@
-lines = list(map(int, open('data.in', 'r').readlines()))
-# with open('data.in', 'r') as f:
-#     for line in [line.strip() for line in f.readlines()]:
+from itertools import product
+
+i_lines = list(map(int, open('data.in').readline().split(',')))
+for a, b in product(range(100), range(100)):
+    lines = i_lines[:]
+    lines[1], lines[2] = a, b
+    i = 0
+    while i < len(lines):
+        if lines[i] == 1:
+            lines[lines[i + 3]] = lines[lines[i + 1]] + lines[lines[i + 2]]
+        elif lines[i] == 2:
+            lines[lines[i + 3]] = lines[lines[i + 1]] * lines[lines[i + 2]]
+        elif lines[i] == 99:
+            break
+        i += 4
+    if lines[0] == 19690720:
+        print(lines[1] * 100 + lines[2])
+        break
